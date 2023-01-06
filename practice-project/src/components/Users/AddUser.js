be9840c782bc24cb25493a56526864e44f6bdca5
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-06 20:05:19 +0530

    Error modal for validation errors

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -27,20 +28,23 @@ const AddUser = (props) => {
     setAge("");
   };
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameHandler}
-          value={username}
-        />
-        <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" onChange={ageHandler} value={age} />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameHandler}
+            value={username}
+          />
+          <label htmlFor="age">Age (years)</label>
+          <input id="age" type="number" onChange={ageHandler} value={age} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
