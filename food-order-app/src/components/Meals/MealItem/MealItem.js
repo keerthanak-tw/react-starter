22893925daf382c012db5362e9f514cd9d76b44a
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-11 21:27:19 +0530

    Input component and refactor meal item form

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,16 +1,19 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
     <li>
-      <div className={classes.meal}>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-      </div>
-      <div>
-
+      <div className={classes["meal-item"]}>
+        <div className={classes.meal}>
+          <h3>{props.name}</h3>
+          <div className={classes.description}>{props.description}</div>
+          <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+          <MealItemForm />
+        </div>
       </div>
     </li>
   );
