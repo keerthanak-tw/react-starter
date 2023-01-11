59e3e9929ee2e3b2d14adc83b415f1e3efc9b905
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-11 20:30:33 +0530

    Header cart button with dummy values

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpeg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
