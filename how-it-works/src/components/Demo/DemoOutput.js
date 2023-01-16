d599fe802c1109b6d082216099b2f8278531816d
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-16 22:13:47 +0530

    useMemo example

--- how-it-works/src/components/Demo/DemoOutput.js
+++ how-it-works/src/components/Demo/DemoOutput.js
@@ -1,6 +1,9 @@
+import React from "react";
+
 const DemoOutput = (props) => {
     console.log('Demo out running');
     return <p>{props.show ? 'This is new!' : ''}</p>
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
+// Previous props are saved and compared. Only if props are changed, component is re-evalated
--- how-it-works/src/components/UI/Button/Button.js
+++ how-it-works/src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
+  console.log('Button running');
   return (
     <button
       type={props.type || 'button'}
@@ -15,4 +16,5 @@ const Button = (props) => {
   );
 };
 
-export default Button;
+export default React.memo(Button);
+// No effect even if wrapped with React.memo as the props like onClick handlers are re-evaluated at parent
