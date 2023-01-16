ba4bb7655dd5762f778e30fcdcc64f0556490177
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-16 22:22:20 +0530

    Method retentition using useCallback

--- how-it-works/src/App.js
+++ how-it-works/src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-import Button from './components/UI/Button/Button';
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState();
+  const [allowToggle, setAllowToggle] = useState();
 
-  console.log('APP RUNNING');
+  console.log("APP RUNNING");
 
-  const toggleParagraph = () => setShowParagraph((prevState) => !prevState);
+  const toggleParagraph = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => setAllowToggle(true);
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {/* {showParagaph && <p>This is new!</p>} */}
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
     </div>
   );
