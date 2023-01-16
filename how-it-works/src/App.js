bc98ae67cc15187f75fd7fd5986572de50de51db
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-16 22:01:12 +0530

    Analyse DOM updates using state

--- how-it-works/src/App.js
+++ how-it-works/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showParagaph, setShowParagraph] = useState();
+
+  const toggleParagraph = () => setShowParagraph((prevState) => !prevState);
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagaph && <p>This is new!</p>}
+      <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
     </div>
   );
 }
