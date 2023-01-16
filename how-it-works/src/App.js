401f075df8a0073eeff9dcdc01d548d58722b311
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-16 22:04:58 +0530

    Rerendering by state and prop update

--- how-it-works/src/App.js
+++ how-it-works/src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-  const [showParagaph, setShowParagraph] = useState();
+  const [showParagraph, setShowParagraph] = useState();
+
+  console.log('APP RUNNING');
 
   const toggleParagraph = () => setShowParagraph((prevState) => !prevState);
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagaph && <p>This is new!</p>}
+      {/* {showParagaph && <p>This is new!</p>} */}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
     </div>
   );
