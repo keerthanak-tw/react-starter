280e592b4cda90b1a39f31cd084592d09bd7c0ad
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-23 20:02:27 +0530

    Add validation for first name field

--- form-validation/src/components/BasicForm.js
+++ form-validation/src/components/BasicForm.js
@@ -1,22 +1,51 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    isValid: firstNameIsValid,
+    hasError: firstNameError,
+    reset: firstNameReset,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+  } = useInput((value) => value.trim() !== "");
+
+  const isFormValid = firstNameIsValid;
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (firstNameError) {
+      return;
+    }
+
+    firstNameReset();
+  };
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="control-group">
+        <div className={`form-control ${firstNameError && "invalid"}`}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameError && (
+            <p className="error-text">First name must not be empty!</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   );
