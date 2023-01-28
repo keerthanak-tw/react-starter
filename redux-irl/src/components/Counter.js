3adca2e1e4bfd2ddeb7d2ad157ba1571e8dd28e8
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-28 15:29:32 +0530

    Adding new state for showing counter

--- redux-irl/src/components/Counter.js
+++ redux-irl/src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
@@ -16,12 +17,14 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
