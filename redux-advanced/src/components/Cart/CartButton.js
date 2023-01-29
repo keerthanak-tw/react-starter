e1138e0a6d6017f50b2a568b6a1aa1c623b416ec
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-29 18:59:50 +0530

    Setting up store slice for managing cart button

--- redux-advanced/src/components/Cart/CartButton.js
+++ redux-advanced/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartClickHandler = () => {
+    dispatch(uiActions.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartClickHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
