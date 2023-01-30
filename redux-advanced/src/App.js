9057b2d274f3f4f29a3c3d0475c9e08fb8654134
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-01-30 19:34:38 +0530

    Sending updated cart to backend

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,15 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-97049-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
