e8f256e673fdfe242c5d7d5f6a3348f4e3b5cd6a
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-03 20:04:55 +0530

    Varying entering and exit times

--- react-animations/src/components/Modal/Modal.js
+++ react-animations/src/components/Modal/Modal.js
@@ -4,8 +4,12 @@ import { Transition } from "react-transition-group";
 import "./Modal.css";
 
 const modal = (props) => {
+  const animationTiming = {
+    enter: 400,
+    exit: 1000,
+  }
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
       {(state) => {
         const cssClasses = [
           "Modal",
