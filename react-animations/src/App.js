e429873c32c0a72b0c39866163d1ba4451712517
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-03 20:02:03 +0530

    Moving transition to modal component

--- react-animations/src/App.js
+++ react-animations/src/App.js
@@ -44,14 +44,7 @@ class App extends Component {
             ></div>
           )}
         </Transition>
-        <Transition
-          in={this.state.modalIsOpen}
-          timeout={300}
-          mountOnEnter
-          unmountOnExit
-        >
-          {(state) => <Modal closed={this.closeModal} show={state} />}
-        </Transition>
+        <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
         {this.state.modalIsOpen && <Backdrop show />}
         <button className="Button" onClick={this.showModal}>
           Open Modal
