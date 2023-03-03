4235324b186f5051c63f45d2c0285ab8ebc14602
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-03 20:14:01 +0530

    CSS transitions to remove class logics

--- react-animations/src/components/Modal/Modal.js
+++ react-animations/src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Transition } from "react-transition-group";
+import { Transition, CSSTransition } from "react-transition-group";
 
 import "./Modal.css";
 
@@ -7,28 +7,22 @@ const modal = (props) => {
   const animationTiming = {
     enter: 400,
     exit: 1000,
-  }
+  };
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames="fade-slide"
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
