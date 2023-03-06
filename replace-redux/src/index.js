3c759083fe4a781636e32ce2f9c757f9a2b95c0b
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-06 19:56:27 +0530

    Products context for passing initial data

--- replace-redux/src/index.js
+++ replace-redux/src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import ProdctsProvider from './context/products-context';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProdctsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+  </ProdctsProvider>,
   document.getElementById('root')
 );
