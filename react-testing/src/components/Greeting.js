be31e51fa1fdb693c056b0be07de79819942f27e
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-08 20:21:33 +0530

    Simple use case of integration test

--- react-testing/src/components/Greeting.js
+++ react-testing/src/components/Greeting.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
   const [changedText, setChangedText] = useState(false);
@@ -9,8 +10,8 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changedText && <p>It's good to see you!</p>}
-      {changedText && <p>I will be here till you toggle it</p>}
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>I will be here till you toggle it</Output>}
       <button onClick={changeTextHandler}>Toggle Text</button>
     </div>
   );
