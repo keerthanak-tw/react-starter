21750d1eea9e3fccf70c842fae9523be2f0f322e
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-13 19:07:55 +0530

    Primitive types

---
+++ typescript/basics.ts
@@ -0,0 +1,8 @@
+// Primitives: number, string, boolean
+// Complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 23;
+let userName: string = 'KK';
+let isInstructor: boolean = true;
