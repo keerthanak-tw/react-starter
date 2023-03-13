1a9fc43eaa1aa199a111ef88b49b29cbcb4a11be
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-13 18:23:14 +0530

    Mocking fetch to respond with mock data

--- react-testing/src/Async.test.js
+++ react-testing/src/Async.test.js
@@ -2,10 +2,17 @@ import { render, screen } from "@testing-library/react";
 import Async from "./components/Async";
 
 describe("Async component", () => {
-    it('renders post if request succeeds', async () => {
-        render(<Async />);
+  it("renders post if request succeeds", async () => {
+    window.fetch = jest.fn().mockResolvedValue({
+        json: () => Promise.resolve([
+          { id: "id1", title: "test title 1" },
+          { id: "id2", title: "test title 2" },
+          { id: "id3", title: "test title 3" },
+        ]),
+      });
+    render(<Async />);
 
-        const listitems = await screen.findAllByRole('listitem');
-        expect(listitems).not.toHaveLength(0);
-    });
+    const listitems = await screen.findAllByRole("listitem");
+    expect(listitems).toHaveLength(3);
+  });
 });
