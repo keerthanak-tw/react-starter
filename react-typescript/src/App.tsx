e277e6b102a725207ea03cb36cd05ce8eaaa91af
Keerthana Kasthuril <keerthana.kasthuril@gmail.com>
2023-03-14 19:45:26 +0530

    Defining todo modal

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TS'),
+    new Todo('End day'),
+  ]
   return (
     <div>
-      <Todos items={['Learn react', 'Learn typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
